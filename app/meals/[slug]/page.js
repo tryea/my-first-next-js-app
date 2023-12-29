f9ad54750a0a426785dddd2ea0ff920c8d0c8425
Ersapta Aristo <aristoersapta@gmail.com>
2023-12-29 11:08:22 +0700

    [main] add community and meals routes

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,7 @@
+export default function MealsSlugPage({ params }) {
+    return (
+        <main>
+            <h1>Meals {params.slug}!</h1>
+        </main>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <img src="/icon.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
+      {/* This is not good since it will remove us from SPA benefit */}
+      {/* <a href="/about">About Us</a> */}
+      <p><Link href="/about">About Us</Link></p>
     </main>
   );
 }
